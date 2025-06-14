7cf213a68b1038a41e3938ead59c9e0e92500809
Harris.Mac <shaohaizhou36@gmail.com>
2025-06-14 19:50:41 +0800

    Initial Commit

---
+++ WordsPaper/WordsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsPaperApp.swift
+//  WordsPaper
+//
+//  Created by HarrisShao on 2025/6/14.
+//
+
+import SwiftUI
+
+@main
+struct WordsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
